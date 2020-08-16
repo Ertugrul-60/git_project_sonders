c62859a206bc794f62ae46255dd62d931ae6cd2b
ertugrul <ertgrltkgz6027@gmail.com>
2020-08-16 21:46:01 +0200

    son commit

---
+++ src/git_project_sonders/OrtakCalisma.java
@@ -0,0 +1,16 @@
+package git_project_sonders;
+
+public class OrtakCalisma {
+
+	public static void main(String[] args) {
+		System.out.println("hamzadan selamlar");
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
